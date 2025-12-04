1a68e56498c430d9ffe9870edd5a868a80d18820
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-12-04 20:28:26 +0000

    Railway (test): added TrainAnonImageUploadTest and TrainAuthenticatedImageUploadTest to the railway/tests_api.py

--- railway/tests/tests_api.py
+++ railway/tests/tests_api.py
@@ -24,6 +24,71 @@ def detail_url(train_id):
     return reverse("railway:train-detail", args=[train_id])
 
 
+class TrainAnonImageUploadTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.train_type = TrainType.objects.create(name="Sample TrainType")
+        self.train = Train.objects.create(
+            name="Sample train",
+            cargo_num=10,
+            places_in_cargo=20,
+            train_type=self.train_type
+        )
+
+    def tearDown(self):
+        self.train.image.delete()
+
+    def test_upload_image_to_train(self):
+        url = image_upload_url(self.train.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            res = self.client.post(
+                url,
+                {"image": ntf},
+                format="multipart"
+            )
+        self.train.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class TrainAuthenticatedImageUploadTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="user@example.ie",
+            password="user-password"
+        )
+        self.client.force_authenticate(user=self.user)
+        self.train_type = TrainType.objects.create(name="Sample TrainType")
+        self.train = Train.objects.create(
+            name="Sample train",
+            cargo_num=10,
+            places_in_cargo=20,
+            train_type=self.train_type
+        )
+
+    def tearDown(self):
+        self.train.image.delete()
+
+    def test_upload_image_to_train(self):
+        url = image_upload_url(self.train.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            res = self.client.post(
+                url,
+                {"image": ntf},
+                format="multipart"
+            )
+        self.train.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
 class TrainImageUploadTests(TestCase):
     def setUp(self):
         self.client = APIClient()
@@ -36,8 +101,8 @@ class TrainImageUploadTests(TestCase):
             name="Sample train",
             cargo_num=10,
             places_in_cargo=20,
-            train_type=self.train_type)
-
+            train_type=self.train_type
+        )
 
     def tearDown(self):
         self.train.image.delete()
@@ -48,7 +113,11 @@ class TrainImageUploadTests(TestCase):
             img = Image.new("RGB", (10, 10))
             img.save(ntf, format="JPEG")
             ntf.seek(0)
-            res = self.client.post(url, {"image": ntf}, format="multipart")
+            res = self.client.post(
+                url,
+                {"image": ntf},
+                format="multipart"
+            )
         self.train.refresh_from_db()
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
@@ -57,8 +126,11 @@ class TrainImageUploadTests(TestCase):
 
     def test_upload_image_bad_request(self):
         url = image_upload_url(self.train.id)
-        res = self.client.post(url, {"image": "not image"}, format="multipart")
-
+        res = self.client.post(
+            url,
+            {"image": "not image"},
+            format="multipart"
+        )
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_post_image_to_train_list(self):
@@ -78,7 +150,6 @@ class TrainImageUploadTests(TestCase):
                 },
                 format="multipart",
             )
-
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         train = Train.objects.get(pk=res.data["id"])
         self.assertFalse(train.image)
@@ -91,5 +162,4 @@ class TrainImageUploadTests(TestCase):
             ntf.seek(0)
             self.client.post(url, {"image": ntf}, format="multipart")
         res = self.client.get(detail_url(self.train.id))
-
         self.assertIn("image", res.data)
